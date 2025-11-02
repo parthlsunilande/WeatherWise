b0a5edcf80120671aafd7a49fef1bd07b21765e3
parthlsunilande <parthlande1111@gmail.com>
2025-11-02 13:32:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from ui.gui import run_gui
+# from core.fetch_weather import get_weather
+# from core.advisor import give_advice
+# from core.alert import send_email_alert
+
+if __name__ == "__main__":
+    run_gui()
